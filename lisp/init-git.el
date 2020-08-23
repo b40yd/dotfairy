fa4abdfc3b55cdeba47f3ef6c0aa43cc86d15691
7ym0n.q6e <bb.qnyd@gmail.com>
2020-08-23 18:21:18 +0800

    add smerge-mode, git-timemachine

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -25,9 +25,74 @@
 ;;; Code:
 
 (use-package magit
-    :ensure t
-    :bind
-    (("C-x g" . magit-status)))
+  :ensure t
+  :bind
+  (("C-x g" . magit-status))
+  :config
+  ;; Access Git forges from Magit
+  (when (executable-find "cc")
+    (use-package forge
+      :demand
+      :init (setq forge-topic-list-columns
+                  '(("#" 5 t (:right-align t) number nil)
+                    ("Title" 60 t nil title  nil)
+                    ("State" 6 t nil state nil)
+                    ("Updated" 10 t nill updated nil)))))
+  )
+
+;; Walk through git revisions of a file
+(use-package git-timemachine
+  :custom-face
+  (git-timemachine-minibuffer-author-face ((t (:inherit success))))
+  (git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+  :bind (:map vc-prefix-map
+              ("t" . git-timemachine))
+  :hook (before-revert . (lambda ()
+                           (when (bound-and-true-p git-timemachine-mode)
+                             (user-error "Cannot revert the timemachine buffer")))))
+
+;; Resolve diff3 conflicts
+(use-package smerge-mode
+  :ensure nil
+  :diminish
+  :pretty-hydra
+  ((:title (pretty-hydra-title "Smerge" 'octicon "diff")
+           :color pink :quit-key "q")
+   ("Move"
+    (("n" smerge-next "next")
+     ("p" smerge-prev "previous"))
+    "Keep"
+    (("b" smerge-keep-base "base")
+     ("u" smerge-keep-upper "upper")
+     ("l" smerge-keep-lower "lower")
+     ("a" smerge-keep-all "all")
+     ("RET" smerge-keep-current "current")
+     ("C-m" smerge-keep-current "current"))
+    "Diff"
+    (("<" smerge-diff-base-upper "upper/base")
+     ("=" smerge-diff-upper-lower "upper/lower")
+     (">" smerge-diff-base-lower "upper/lower")
+     ("R" smerge-refine "refine")
+     ("E" smerge-ediff "ediff"))
+    "Other"
+    (("C" smerge-combine-with-next "combine")
+     ("r" smerge-resolve "resolve")
+     ("k" smerge-kill-current "kill")
+     ("ZZ" (lambda ()
+             (interactive)
+             (save-buffer)
+             (bury-buffer))
+      "Save and bury buffer" :exit t))))
+  :bind (:map smerge-mode-map
+              ("C-c g m" . smerge-mode-hydra/body))
+  :hook ((find-file . (lambda ()
+                        (save-excursion
+                          (goto-char (point-min))
+                          (when (re-search-forward "^<<<<<<< " nil t)
+                            (smerge-mode 1)))))
+         (magit-diff-visit-file . (lambda ()
+                                    (when smerge-mode
+                                      (hydra-smerge/body))))))
 
 (provide 'init-git)
 ;;; init-git.el ends here
