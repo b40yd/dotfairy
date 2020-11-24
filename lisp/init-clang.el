f3b7a1d831168f256c5bc0979f68771a3f7a1875
7ym0n.q6e <bb.qnyd@gmail.com>
2020-11-24 14:59:21 +0800

    change clang keymap

--- lisp/init-clang.el
+++ lisp/init-clang.el
@@ -28,7 +28,7 @@
 (use-package cc-mode
   :ensure nil
   :bind (:map c-mode-base-map
-         ("C-c c" . compile))
+         ("C-c C-c" . compile))
   :hook (c-mode-common . (lambda () (c-set-style "stroustrup")))
   :init (setq-default c-basic-offset 4)
   :config
