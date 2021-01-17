8b222621ec6048645c4c6c02d5f7a1a2a152037c
7ym0n.q6e <bb.qnyd@gmail.com>
2021-01-17 22:56:58 +0800

    add calendar

---
+++ lisp/init-calendar.el
@@ -0,0 +1,52 @@
+;;; init-calendar.el ---                                   -*- lexical-binding: t; -*-
+
+;; Copyright © 2021, 7ym0n.q6e, all rights reserved.
+
+;; Author: 7ym0n.q6e <bb.qnyd@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(use-package cal-china-x
+  :after calendar
+  :commands cal-china-x-setup
+  :init (cal-china-x-setup)
+  :config
+  ;; Holidays
+  (setq calendar-mark-holidays-flag t
+        cal-china-x-important-holidays cal-china-x-chinese-holidays
+        cal-china-x-general-holidays '((holiday-lunar 1 15 "元宵节")
+                                       (holiday-lunar 7 7 "七夕节")
+                                       (holiday-fixed 3 8 "妇女节")
+                                       (holiday-fixed 3 12 "植树节")
+                                       (holiday-fixed 5 4 "青年节")
+                                       (holiday-fixed 6 1 "儿童节")
+                                       (holiday-fixed 9 10 "教师节"))
+        holiday-other-holidays '((holiday-fixed 2 14 "情人节")
+                                 (holiday-fixed 4 1 "愚人节")
+                                 (holiday-fixed 12 25 "圣诞节")
+                                 (holiday-float 5 0 2 "母亲节")
+                                 (holiday-float 6 0 3 "父亲节")
+                                 (holiday-float 11 4 4 "感恩节"))
+        calendar-holidays (append cal-china-x-important-holidays
+                                  cal-china-x-general-holidays
+                                  holiday-other-holidays)))
+
+(provide 'init-calendar)
+;;; init-calendar.el ends here
