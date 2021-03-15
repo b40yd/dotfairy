16b9775354a86aa7488f9dea0918a16b97836cd7
7ym0n.q6e <bb.qnyd@gmail.com>
2021-03-15 22:02:26 +0800

    remove lsp face

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -28,32 +28,7 @@
   :ensure t
   :defines (lsp-clients-python-library-directories
             lsp-rust-server)
-  :custom-face
-  (lsp-headerline-breadcrumb-path-error-face
-   ((t :underline (:style wave :color ,(face-foreground 'error))
-       :inherit lsp-headerline-breadcrumb-path-face)))
-  (lsp-headerline-breadcrumb-path-warning-face
-   ((t :underline (:style wave :color ,(face-foreground 'warning))
-       :inherit lsp-headerline-breadcrumb-path-face)))
-  (lsp-headerline-breadcrumb-path-info-face
-   ((t :underline (:style wave :color ,(face-foreground 'success))
-       :inherit lsp-headerline-breadcrumb-path-face)))
-  (lsp-headerline-breadcrumb-path-hint-face
-   ((t :underline (:style wave :color ,(face-foreground 'success))
-       :inherit lsp-headerline-breadcrumb-path-face)))
 
-  (lsp-headerline-breadcrumb-symbols-error-face
-   ((t :inherit lsp-headerline-breadcrumb-symbols-face
-       :underline (:style wave :color ,(face-foreground 'error)))))
-  (lsp-headerline-breadcrumb-symbols-warning-face
-   ((t :inherit lsp-headerline-breadcrumb-symbols-face
-       :underline (:style wave :color ,(face-foreground 'warning)))))
-  (lsp-headerline-breadcrumb-symbols-info-face
-   ((t :inherit lsp-headerline-breadcrumb-symbols-face
-       :underline (:style wave :color ,(face-foreground 'success)))))
-  (lsp-headerline-breadcrumb-symbols-hint-face
-   ((t :inherit lsp-headerline-breadcrumb-symbols-face
-       :underline (:style wave :color ,(face-foreground 'success)))))
   :hook ((prog-mode . (lambda ()
                         (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode)
                           (lsp-deferred))))
