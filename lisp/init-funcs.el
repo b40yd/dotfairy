135c76e39f6b81acfc1c6d799db35e0b916c72f9
7ym0n.q6e <bb.qnyd@gmail.com>
2021-03-18 17:23:58 +0800

    add delete rename current buffer file.

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -268,5 +268,28 @@ Return the fastest package archive."
         (error "Cannot open tramp file")
       (browse-url (concat "file://" file-name)))))
 
+(defun delete-this-file ()
+  "Delete the current file, and kill the buffer."
+  (interactive)
+  (unless (buffer-file-name)
+    (error "No file is currently being edited"))
+  (when (yes-or-no-p (format "Really delete '%s'?"
+                             (file-name-nondirectory buffer-file-name)))
+    (delete-file (buffer-file-name))
+    (kill-this-buffer)))
+
+(defun rename-this-file (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (unless filename
+      (error "Buffer '%s' is not visiting a file!" name))
+    (progn
+      (when (file-exists-p filename)
+        (rename-file filename new-name 1))
+      (set-visited-file-name new-name)
+      (rename-buffer new-name))))
+
 (provide 'init-funcs)
 ;;; init-funcs.el ends here
