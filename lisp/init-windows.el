424a39b516b8c7dfca6ac24726ec9113605fb50b
7ym0n <bb.qnyd@gmail.com>
2021-08-29 15:07:47 +0800

    move treemacs to init-project.el

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -132,61 +132,6 @@
                 (interactive)
                 (aw--select-window (1+ n))))))
 
-
-(use-package treemacs
-  :commands (treemacs-follow-mode
-             treemacs-filewatch-mode
-             treemacs-fringe-indicator-mode
-             treemacs-git-mode)
-  :bind (([f8]        . treemacs)
-         ("M-0"       . treemacs-select-window)
-         ("C-x 1"     . treemacs-delete-other-windows)
-         ("C-x t 1"   . treemacs-delete-other-windows)
-         ("C-x t t"   . treemacs)
-         ("C-x t b"   . treemacs-bookmark)
-         ("C-x t C-t" . treemacs-find-file)
-         ("C-x t M-t" . treemacs-find-tag)
-         :map treemacs-mode-map
-         ([mouse-1]   . treemacs-single-click-expand-action))
-  :config
-  (setq treemacs-collapse-dirs           (if treemacs-python-executable 3 0)
-        treemacs-sorting                 'alphabetic-asc
-        treemacs-follow-after-init       t
-        treemacs-is-never-other-window   t
-        treemacs-silent-filewatch        t
-        treemacs-silent-refresh          t
-        treemacs-width                   30
-        treemacs-persist-file (concat dotfairy-cache-dir "treemacs-persist")
-        treemacs-last-error-persist-file (concat dotfairy-cache-dir "treemacs-last-error-persist"))
-  (treemacs-follow-mode t)
-  (treemacs-filewatch-mode t)
-  (pcase (cons (not (null (executable-find "git")))
-               (not (null (executable-find "python3"))))
-    (`(t . t)
-     (treemacs-git-mode 'deferred))
-    (`(t . _)
-     (treemacs-git-mode 'simple)))
-
-  ;; Projectile integration
-  (use-package treemacs-projectile
-    :after projectile
-    :bind (:map projectile-command-map
-           ("h" . treemacs-projectile)))
-
-  (use-package treemacs-magit
-    :after magit
-    :commands treemacs-magit--schedule-update
-    :hook ((magit-post-commit
-            git-commit-post-finish
-            magit-post-stage
-            magit-post-unstage)
-           . treemacs-magit--schedule-update))
-  (use-package treemacs-persp
-    :after persp-mode
-    :demand t
-    :functions treemacs-set-scope-type
-    :config (treemacs-set-scope-type 'Perspectives)))
-
 ;; Enforce rules for popups
 (use-package shackle
   :hook (after-init . shackle-mode)
