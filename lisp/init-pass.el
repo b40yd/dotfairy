9929da15cb8f91bd2938edeb7e1cfbbdec8ad036
Tag Bao <bb.qnyd@gmail.com>
2021-09-17 09:50:03 +0800

    fixed missing init-pass, remove old config

---
+++ lisp/init-pass.el
@@ -0,0 +1,32 @@
+;;; init-pass.el ---                                   -*- lexical-binding: t; -*-
+
+;; Copyright © 2021, 7ym0n, all rights reserved.
+
+;; Author: 7ym0n <bb.qnyd@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+
+;;; Code:
+(use-package pass
+  :config
+  (setq pass-username-fallback-on-filename t))
+
+(provide 'init-pass)
+;;; init-pass.el ends here
