f158bfb473d3832c7f1e6b086e4ccbc43531be6c
Tag Bao <bb.qnyd@gmail.com>
2021-09-19 15:27:42 +0800

    add vterm-posframe.

--- lisp/init-vterm.el
+++ lisp/init-vterm.el
@@ -108,10 +108,60 @@
     (use-package vterm
       :commands (vterm--internal)
       :init
-      (setq vterm-always-compile-module t))))
+      (setq vterm-always-compile-module t)
+      :config
+      (with-no-warnings
+        (defvar vterm-posframe--frame nil)
+
+        (defun vterm-posframe-hidehandler (_)
+          "Hidehandler used by `vterm-posframe-toggle'."
+          (not (eq (selected-frame) posframe--frame)))
+
+        (defun vterm-posframe-toggle ()
+          "Toggle `vterm' child frame."
+          (interactive)
+          (let ((buffer (vterm--internal #'ignore 100)))
+            (if (and vterm-posframe--frame
+                     (frame-live-p vterm-posframe--frame)
+                     (frame-visible-p vterm-posframe--frame))
+                (progn
+                  (posframe-hide buffer)
+                  ;; Focus the parent frame
+                  (select-frame-set-input-focus (frame-parent vterm-posframe--frame)))
+              (let ((width  (max 80 (/ (frame-width) 2)))
+                    (height (/ (frame-height) 2)))
+                (setq vterm-posframe--frame
+                      (posframe-show
+                       buffer
+                       :poshandler #'posframe-poshandler-frame-center
+                       :hidehandler #'vterm-posframe-hidehandler
+                       :left-fringe 8
+                       :right-fringe 8
+                       :width width
+                       :height height
+                       :min-width width
+                       :min-height height
+                       :internal-border-width 3
+                       :internal-border-color (face-foreground 'font-lock-comment-face nil t)
+                       :background-color (face-background 'tooltip nil t)
+                       :override-parameters '((cursor-type . t))
+                       :accept-focus t))
+                ;; Blink cursor
+                (with-current-buffer buffer
+                  (save-excursion
+                    (vterm-clear))
+                  (setq-local cursor-type 'bar))
+                ;; Focus the child frame
+                (select-frame-set-input-focus vterm-posframe--frame)))))
+        (bind-key "C-`" #'vterm-posframe-toggle)))))
 ;; Shell Pop
 (use-package shell-pop
-  :bind (("C-`" . shell-pop))
+  :bind (("C-`" . (lambda ()
+                    (interactive)
+                    (if (fboundp 'vterm-posframe-toggle)
+                        (vterm-posframe-toggle)
+                      (shell-pop nil))))
+         ([f9] . shell-pop))
   :init (setq shell-pop-window-size 30
               shell-pop-shell-type
               (cond ((fboundp 'vterm) '("vterm" "*vterm*" #'vterm))
