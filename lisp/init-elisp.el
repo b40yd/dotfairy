33d2d5ffbde698c26fcf22c2be1a07d3caea0e49
7ym0n <bb.qnyd@gmail.com>
2021-11-04 09:51:09 +0800

    fix posframe--set-frame-size

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -258,6 +258,8 @@ Lisp function does not specify a special indentation."
                     :postion (point)
                     :left-fringe 8
                     :right-fringe 8
+                    :max-width (round (* (frame-width) 0.62))
+                    :max-height (round (* (frame-height) 0.62))
                     :poshandler #'posframe-poshandler-point-bottom-left-corner-upward
                     :internal-border-width 1
                     :internal-border-color (face-foreground 'font-lock-comment-face nil t)
