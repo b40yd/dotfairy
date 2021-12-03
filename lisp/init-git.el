8f7fc68ad87cee92581476025030be679c557456
7ym0n <bb.qnyd@gmail.com>
2021-12-03 09:58:23 +0800

    remove blamer
    It's annoying sometimes.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -276,17 +276,8 @@ kill all magit buffers for this repo."
 ;; Open github/gitlab/bitbucket page
 (use-package browse-at-remote
   :bind (:map vc-prefix-map
-         ("C-c v B" . browse-at-remote)))
+         ("." . browse-at-remote)))
 
-;; Show git blame info
-(use-package blamer
-  :custom-face (blamer-face ((t (:inherit completions-annotations :height 0.85))))
-  :hook (after-init . global-blamer-mode)
-  :init (setq blamer-idle-time 0.5
-              blamer-min-offset 40
-              blamer-author-formatter "%s "
-              blamer-datetime-formatter "[%s] "
-              blamer-commit-formatter "- %s"))
 
 ;; Git related modes
 (use-package git-modes)
