3270c3aa2dd5528149efeeaec153cb391459c974
7ym0n <bb.qnyd@gmail.com>
2022-02-28 09:42:32 +0800

    Minor fix

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -166,7 +166,7 @@
            . treemacs-magit--schedule-update))
   (use-package treemacs-persp
     :after persp-mode
-    :demand t
+    :demand
     :functions treemacs-set-scope-type
     :config (treemacs-set-scope-type 'Perspectives)))
 
