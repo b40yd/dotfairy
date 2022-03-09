0a2e1f5458859a575add9288b03238294bbb43a3
7ym0n <bb.qnyd@gmail.com>
2022-03-09 09:34:54 +0800

    update popper rules.

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -229,14 +229,15 @@
   (setq popper-reference-buffers
         '("\\*Messages\\*"
           "Output\\*$" "\\*Pp Eval Output\\*$"
+          "\\*Compile-Log\\*"
           "\\*Completions\\*"
           "\\*Warnings\\*"
           "\\*Async Shell Command\\*"
           "\\*Apropos\\*"
           "\\*Backtrace\\*"
-          ;; "\\*Calendar\\*"              ; FIXME: https://github.com/karthink/popper/issues/29
-
+          "\\*Calendar\\*"
           bookmark-bmenu-mode
+          comint-mode
           compilation-mode
           help-mode helpful-mode
           tabulated-list-mode
@@ -275,6 +276,10 @@
           rustic-cargo-outdated-mode rustic-cargo-test-moed))
   (with-eval-after-load 'projectile
     (setq popper-group-function #'popper-group-by-projectile))
+  (when (display-grayscale-p)
+    (setq popper-mode-line
+          '(:eval
+            (format " %s " (all-the-icons-octicon "pin" :height 0.9 :v-adjust 0.0 :face 'mode-line-emphasis)))))
   (setq popper-echo-dispatch-actions t)
   :config
   (popper-echo-mode 1)
