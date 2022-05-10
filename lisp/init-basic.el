891acf78931a5b728d649917d0351076fbae2c76
7ym0n <bb.qnyd@gmail.com>
2022-05-10 16:28:14 +0800

    Make list-processes be compatible with 26.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -213,7 +213,7 @@
 
     (defun my-list-processes--prettify ()
       "Prettify process list."
-      (let ((entries tabulated-list-entries))
+      (when-let ((entries tabulated-list-entries))
         (setq tabulated-list-entries nil)
         (dolist (p (process-list))
           (when-let* ((val (cadr (assoc p entries)))
@@ -233,8 +233,10 @@
                       (buf-label (aref val 3))
                       (tty (list (aref val 4) 'face 'font-lock-doc-face))
                       (thread (list (aref val 5) 'face 'font-lock-doc-face))
-                      (cmd (list (aref val 6) 'face 'completions-annotations)))
-            (push (list p (vector icon name pid status buf-label tty thread cmd))
+                      (cmd (list (aref val (if (>= emacs-major-version 27) 6 5)) 'face 'completions-annotations)))
+            (push (list p (if (>= emacs-major-version 27)
+                              (vector icon name pid status buf-label tty thread cmd)
+                            (vector icon name pid status buf-label tty cmd)))
 		          tabulated-list-entries)))))
     (advice-add #'list-processes--refresh :after #'my-list-processes--prettify)))
 
