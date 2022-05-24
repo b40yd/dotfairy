9b0b9d53716543da78904c4f1ca487ce64af5ed2
7ym0n <bb.qnyd@gmail.com>
2022-05-24 18:56:27 +0800

    add init-yasnippet.el

---
+++ lisp/init-yasnippet.el
@@ -0,0 +1,35 @@
+;;; init-yasnippet.el ---                                   -*- lexical-binding: t; -*-
+
+;; Copyright © 2022, 7ym0n, all rights reserved.
+
+;; Author: 7ym0n <bb.qnyd@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :hook (after-init . yas-global-mode))
+
+(use-package yasnippet-snippets
+  :after yasnippet)
+
+(provide 'init-yasnippet)
+;;; init-yasnippet.el ends here
