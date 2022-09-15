3437a915f5de7326e7008fe265eaf844faaa7ec3
7ym0n.q6e <bb.qnyd@gmail.com>
2022-09-15 13:54:35 +0800

    Removing advices and hooks.
    project-root may not be defined.
    fixed all-the-icons-dired.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -80,29 +80,20 @@
                             (all-the-icons-dired-mode))))
     :config
     (with-no-warnings
-      (defun my-all-the-icons-dired--refresh ()
-        "Display the icons of files in a dired buffer."
-        (all-the-icons-dired--remove-all-overlays)
-        ;; NOTE: don't display icons it too many items
-        (if (<= (count-lines (point-min) (point-max)) 1000)
-            (save-excursion
-              (goto-char (point-min))
-              (while (not (eobp))
-                (when (dired-move-to-filename nil)
-                  (let ((file (file-local-name (dired-get-filename 'relative 'noerror))))
-                    (when file
-                      (let ((icon (if (file-directory-p file)
-                                      (all-the-icons-icon-for-dir file
-                                                                  :face 'all-the-icons-dired-dir-face
-                                                                  :height 0.9
-                                                                  :v-adjust all-the-icons-dired-v-adjust)
-                                    (all-the-icons-icon-for-file file :height 0.9 :v-adjust all-the-icons-dired-v-adjust))))
-                        (if (member file '("." ".."))
-                            (all-the-icons-dired--add-overlay (point) "  \t")
-                          (all-the-icons-dired--add-overlay (point) (concat icon "\t")))))))
-                (forward-line 1)))
-          (message "Not display icons because of too many items.")))
-      (advice-add #'all-the-icons-dired--refresh :override #'my-all-the-icons-dired--refresh)))
+      (defun my-all-the-icons-dired--icon (file)
+        "Return the icon for FILE."
+        (if (file-directory-p file)
+            (all-the-icons-icon-for-dir file
+                                        :height 0.9
+                                        :face 'all-the-icons-dired-dir-face
+                                        :v-adjust all-the-icons-dired-v-adjust)
+          (apply 'all-the-icons-icon-for-file file
+                 (append
+                  '(:height 0.9)
+                  `(:v-adjust ,all-the-icons-dired-v-adjust)
+                  (when all-the-icons-dired-monochrome
+                    `(:face ,(face-at-point)))))))
+      (advice-add #'all-the-icons-dired--icon :override #'my-all-the-icons-dired--icon)))
 
   ;; Extra Dired functionality
   ;; C-x C-q into modify mode
