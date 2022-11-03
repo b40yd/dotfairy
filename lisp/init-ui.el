79e3772df6ad9a831b142b3e165edb742d7539d5
b40yd <bb.qnyd@gmail.com>
2022-11-03 21:56:23 +0800

    Add python environment manager.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -119,6 +119,7 @@
 ;; Mode-line
 (use-package doom-modeline
   :hook (after-init . doom-modeline-mode)
+  :autoload (+modeline-update-env-in-all-windows-h +modeline-clear-env-in-all-windows-h)
   :init
   (setq doom-modeline-icon display-icon
         doom-modeline-height 1
@@ -129,6 +130,25 @@
   ;; Prevent flash of unstyled modeline at startup
   (unless after-init-time
     (setq-default mode-line-format nil))
+  :config
+;;;###autoload
+  (defun +modeline-update-env-in-all-windows-h (&rest _)
+    "Update version strings in all buffers."
+    (dolist (window (window-list))
+      (with-selected-window window
+        (when (fboundp 'doom-modeline-update-env)
+          (doom-modeline-update-env))
+        (force-mode-line-update))))
+
+;;;###autoload
+  (defun +modeline-clear-env-in-all-windows-h (&rest _)
+    "Blank out version strings in all buffers."
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (setq doom-modeline-env--version
+              (bound-and-true-p doom-modeline-load-string))))
+    (force-mode-line-update t))
+
   :bind (:map doom-modeline-mode-map
          ("<f6>" . doom-modeline-hydra/body))
   :pretty-hydra
