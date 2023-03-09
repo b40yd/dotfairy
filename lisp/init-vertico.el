544e9106a17789def327fc925e7093514d6c9fbf
b40yd <bb.qnyd@gmail.com>
2023-03-09 15:03:27 +0800

    Change +vertico-file-search

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -374,62 +374,24 @@ Supports exporting consult-grep to wgrep, file to wdeired, and consult-location 
             '(projectile-switch-project . project-file)))
 
 
-(cl-defun +vertico-file-search (&key query in all-files (recursive t) prompt args)
+(defun +vertico-file-search (&optional directory query)
   "Conduct a file search using ripgrep.
-:query STRING
+query STRING
   Determines the initial input to search for.
-:in PATH
-  Sets what directory to base the search out of. Defaults to the current project's root.
-:recursive BOOL
-  Whether or not to search files recursively from the base directory."
-  (declare (indent defun))
-  (unless (executable-find "rg")
-    (user-error "Couldn't find ripgrep in your PATH"))
-  (require 'consult)
-  (setq deactivate-mark t)
-  (let* ((project-root (or (dotfairy-project-root) default-directory))
-         (directory (or in project-root))
-         (consult-ripgrep-args
-          (concat "rg "
-                  (if all-files "-uu ")
-                  (unless recursive "--maxdepth 1 ")
-                  "--null --line-buffered --color=never --max-columns=1000 "
-                  "--path-separator /   --smart-case --no-heading --line-number "
-                  "--hidden -g !.git -g !.svn -g !.hg -g !.ccls-cache "
-                  (mapconcat #'shell-quote-argument args " ")
-                  "."))
-         (prompt (if (stringp prompt) (string-trim prompt) "Search"))
-         (query (or query
+directory PATH
+  Sets what directory to base the search out of. Defaults to the current project's root."
+
+  (let* ((query (or query
                     (when (dotfairy-region-active-p)
-                      (regexp-quote (dotfairy-thing-at-point-or-region)))))
-         (consult-async-split-style consult-async-split-style)
-         (consult-async-split-styles-alist consult-async-split-styles-alist))
-    ;; Change the split style if the initial query contains the separator.
-    (when query
-      (cl-destructuring-bind (&key type separator initial _function)
-          (consult--async-split-style)
-        (pcase type
-          (`separator
-           (replace-regexp-in-string (regexp-quote (char-to-string separator))
-                                     (concat "\\" (char-to-string separator))
-                                     query t t))
-          (`perl
-           (when (string-match-p initial query)
-             (setf (alist-get 'perlalt consult-async-split-styles-alist)
-                   `(:initial ,(or (cl-loop for char in (list "%" "@" "!" "&" "/" ";")
-                                            unless (string-match-p char query)
-                                            return char)
-                                   "%")
-                     :type perl)
-                   consult-async-split-style 'perlalt))))))
-    (consult--grep prompt #'consult--ripgrep-make-builder directory query)))
+                      (regexp-quote (dotfairy-thing-at-point-or-region))))))
+    (consult-ripgrep directory query)))
 
 (defun +vertico/project-search (&optional arg initial-query directory)
   "Peforms a live project search from the project root using ripgrep.
 If ARG (universal argument), include all files, even hidden or compressed ones,
 in the search."
   (interactive "P")
-  (+vertico-file-search :query initial-query :in directory :all-files arg))
+  (+vertico-file-search directory initial-query))
 
 (defun +vertico/project-search-from-cwd (&optional arg initial-query)
   "Performs a live project search from the current directory.
