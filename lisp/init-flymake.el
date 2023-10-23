ba55f123c37825ca7c3d679c0425dd6c1741d4bc
b40yd <bb.qnyd@gmail.com>
2023-10-23 14:51:44 +0800

    Refactor.

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -35,11 +35,6 @@
   :config (setq elisp-flymake-byte-compile-load-path
                 (append elisp-flymake-byte-compile-load-path load-path)))
 
-(use-package sideline-flymake
-  :diminish sideline-mode
-  :hook (flymake-mode . sideline-mode)
-  :init (setq sideline-flymake-display-mode 'point
-              sideline-backends-right '(sideline-flymake)))
 
 (provide 'init-flymake)
 ;;; init-flymake.el ends here
