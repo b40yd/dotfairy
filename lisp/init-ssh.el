96f8c77ce93afa084e00c4f61094dd86a6990d03
b40yd <bb.qnyd@gmail.com>
2024-03-28 11:24:39 +0800

    Add ssh deploy.

--- lisp/init-ssh.el
+++ lisp/init-ssh.el
@@ -27,12 +27,57 @@
 ;; It's like `xshell', `mobaxterm' or other tools same work.
 
 ;;; Code:
+(require 'init-const)
+(require 'init-custom)
+(require 'init-funcs)
+
 (use-package ssh-manager
   :ensure nil
   :quelpa (ssh-manager :fetcher github :repo "b40yd/dotfairy-ssh-manager")
   :init
   (setq ssh-manager-sshpass-path (expand-file-name "sshpass" dotfairy-local-dir)
         ssh-manager-sshpass-bin (concat ssh-manager-sshpass-path "/bin/sshpass")))
+
+
+(use-package ssh-deploy
+  :commands (ssh-deploy-upload-handler
+             ssh-deploy-upload-handler-forced
+             ssh-deploy-diff-handler
+             ssh-deploy-browse-remote-handler
+             ssh-deploy-remote-changes-handler)
+  :init
+  (setq ssh-deploy-revision-folder (concat dotfairy-cache-dir "ssh-revisions/")
+        ssh-deploy-on-explicit-save 1
+        ssh-deploy-automatically-detect-remote-changes nil)
+
+  ;; Make these safe as file-local variables
+  (dolist (sym '((ssh-deploy-root-local . stringp)
+                 (ssh-deploy-root-remote . stringp)
+                 (ssh-deploy-script . functionp)
+                 (ssh-deploy-on-explicit-save . booleanp)
+                 (ssh-deploy-force-on-explicit-save . booleanp)
+                 (ssh-deploy-async . booleanp)
+                 (ssh-deploy-exclude-list . listp)))
+    (put (car sym) 'safe-local-variable (cdr sym)))
+
+  ;; Maybe auto-upload on save
+  (add-hook! 'after-save-hook
+    (defun +upload-init-after-save-h ()
+      (when (and (bound-and-true-p ssh-deploy-root-remote)
+                 (integerp ssh-deploy-on-explicit-save)
+                 (> ssh-deploy-on-explicit-save 0))
+        (ssh-deploy-upload-handler ssh-deploy-force-on-explicit-save))))
+
+  ;; Enable ssh-deploy if variables are set, and check for changes on open file
+  ;; (if possible)
+  (add-hook! 'find-file-hook
+    (defun +upload-init-find-file-h ()
+      (when (bound-and-true-p ssh-deploy-root-remote)
+        (require 'ssh-deploy)
+        (unless ssh-deploy-root-local
+          (setq ssh-deploy-root-local (dotfairy-project-root)))
+        (when ssh-deploy-automatically-detect-remote-changes
+          (ssh-deploy-remote-changes-handler))))))
 
 (provide 'init-ssh)
 ;;; ssh-manager.el ends here
