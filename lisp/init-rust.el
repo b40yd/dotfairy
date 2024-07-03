4cc40cb23c293cda0f9262de545b2184d4d5dd8d
b40yd <bb.qnyd@gmail.com>
2024-07-03 17:42:02 +0800

    Change rust config.

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -5,10 +5,11 @@
   (add-to-list 'projectile-project-root-files "Cargo.toml"))
 
 (use-package rust-mode
-  :mode ("\\.rs$" . rust-mode)
+  :mode ("\\.rs\\'" . rustic-mode)
   :init (setq rust-format-on-save t
               rust-mode-treesitter-derive t)
   :config
+
   (set-ligatures! 'rust-mode
     ;; Functional
     :def "fn"
@@ -22,7 +23,17 @@
     :in "in"
     :and "&&" :or "||"
     :for "for"
-    :return "return"))
+    :return "return")
+
+  ;; HACK: `global-treesit-auto-mode' will override `rust-mode'.
+  (define-derived-mode rustic-mode rust-mode "Rust"
+    "Major mode for Rust code.
+
+\\{rust-mode-map}")
+
+  (when (centaur-treesit-available-p)
+    (setq auto-mode-alist (delete '("\\.rs\\'" . rust-mode) auto-mode-alist))
+    (setq auto-mode-alist (delete '("\\.rs\\'" . rust-ts-mode) auto-mode-alist))))
 
 (use-package rust-playground)
 
