b5e603d1f0bd48239ac81728c51b25a8ee701dad
b40yd <bb.qnyd@gmail.com>
2025-04-21 09:27:35 +0800

    fix(org): allow refiling to top level

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -366,7 +366,7 @@ PATH (a string) can be an url, a local file path, or a base64 encoded datauri."
 If prefix ARG, copy instead of move."
     (interactive "P")
     (let ((org-refile-targets `((,file :maxlevel . 10)))
-          (org-refile-use-outline-path nil)
+          (org-refile-use-outline-path 'file)
           (org-refile-keep arg)
           current-prefix-arg)
       (call-interactively #'org-refile)))
