47a2896b756d4f2f31f0acd54e49e898a6e8e95b
b40yd <bb.qnyd@gmail.com>
2025-04-27 09:42:59 +0800

    adjust initial frame size

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -42,8 +42,8 @@
 ;; Initial frame
 (setq initial-frame-alist '((top . 0.5)
                             (left . 0.5)
-                            (width . 0.63)
-                            (height . 0.8)
+                            (width . 0.7)
+                            (height . 0.85)
                             (fullscreen)))
 
 ;; Title
