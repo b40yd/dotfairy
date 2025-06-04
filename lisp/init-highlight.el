522ddd72bdb51e20f938ab6213da5f398d258750
b40yd <bb.qnyd@gmail.com>
2025-06-04 09:37:59 +0800

    adjust face: show-paren-match

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -359,9 +359,9 @@ be enabled. If any function returns non-nil, the mode will not be activated."
   :ensure nil
   :custom-face
   (show-paren-match ((((class color) (background light))
-                      (:box (:line-width (-1 . -1) :color "gray70")))
+                      (:box (:line-width (-1 . -1) :color "gray73")))
                      (((class color) (background dark))
-                      (:box (:line-width (-1 . -1) :color "gray50")))))
+                      (:box (:line-width (-1 . -1) :color "gray56")))))
   :hook (after-init . show-paren-mode)
   :init (setq show-paren-when-point-inside-paren t
               show-paren-when-point-in-periphery t)
