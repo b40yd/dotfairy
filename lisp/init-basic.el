a0f8e15bd221d3b58d4eaaba3cdabd8536d369b4
b40yd <bb.qnyd@gmail.com>
2025-11-18 09:47:38 +0800

    feat(emacs): Optimize package handling for Windows systems
    Introduced improvements to `load-path` management and package
    installations for Windows systems to enhance Emacs startup
    performance. Added custom directory strategies for
    `package-user-dir` and streamlined package deletion. Adjusted async
    package compilation mode to exclude Windows systems.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -235,6 +235,13 @@
     (revert-buffer t t)
     (message "Reverted this buffer")))
 
+(use-package async
+  :functions (async-bytecomp-package-mode dired-async-mode)
+  :init
+  (unless IS-WINDOWS
+    (async-bytecomp-package-mode 1))
+  (dired-async-mode 1))
+
 ;; Child frame
 (when (childframe-workable-p)
   (use-package posframe
