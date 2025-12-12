5a8867a7237f688864c59706ced8f5646171f455
b40yd <bb.qnyd@gmail.com>
2025-12-12 09:52:18 +0800

    remove calfw

--- site-lisp/custom-keybinds.el
+++ site-lisp/custom-keybinds.el
@@ -28,9 +28,6 @@
 (autoload 'org-capture-goto-target "org-capture" nil t)
 (map! :leader
   (:prefix-map ("a" . "appliction")
-   (:prefix ("C" . "Calendar")
-    :desc "Open calendar"              "o" #'+calendar/open-calendar
-    :desc "Open git calendar"          "g" #'cfw:git-open-calendar)
    (:prefix ("c" . "Command log")
     :desc "Enable command log mode"    "e" #'command-log-mode
     :desc "Toggle command log buffer"  "g" #'clm/toggle-command-log-buffer)
