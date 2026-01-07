458626b33f14f1a68994845b3e6a4140fd16b59f
b40yd <bb.qnyd@gmail.com>
2026-01-07 12:27:15 +0800

    add uv mode.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -156,5 +156,37 @@
         :desc "Cython compile buffer"    "c" #'cython-compile))
 
 
+(use-package uv-mode
+  :after python
+  :config
+  ;;;###autoload
+  (defvar +python--uv-project nil)
+
+  ;;;###autoload
+  (defvar +python--uv-version nil)
+
+  ;;;###autoload
+  (defun +python-uv-mode-set-auto-h ()
+    "Set pyenv-mode version from buffer-local variable."
+    (when (derived-mode-p 'python-mode 'python-ts-mode)
+      (unless (local-variable-p '+python--uv-project)
+        (setq-local +python--uv-project
+                    (uv-mode-full-path (uv-mode-root))))
+      (if (and +python--uv-project
+               (file-exists-p +python--uv-project))
+          (unless (eq +python--uv-project (getenv "VIRTUAL_ENV"))
+            (setq-local +python--uv-version (uv-mode-version))
+            (uv-mode-set))
+        (kill-local-variable '+python--uv-project)
+        (kill-local-variable '+python--uv-version)
+        (uv-mode-unset))))
+
+  ;; HACK: A faster (cached) version of the mode-line segment. See
+  ;;   `+python-uv-mode-set-auto-h'.
+  (setq uv-mode-mode-line-format '(+python--uv-version ("UV:" +python--uv-version " ")))
+  (if (executable-find "uv") (uv-mode +1))
+  (add-hook! '(python-mode-local-vars-hook python-ts-mode-local-vars-hook)
+             #'+python-uv-mode-set-auto-h)
+  (add-hook 'doom-switch-buffer-hook #'+python-uv-mode-set-auto-h))
 (provide 'init-python)
 ;;; init-python.el ends here
